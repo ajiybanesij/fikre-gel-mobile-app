a727fc428094459a9c9e18f6c00ba47f3de9e80e
Nesij <nesij.korkmaz@argedor.com>
2020-11-13 17:27:05 +0300

    New Page and Popular Page added

--- App.js
+++ App.js
@@ -1,49 +1,45 @@
-import React from 'react';
-import {View, Text, StyleSheet,FlatList,RefreshControl} from 'react-native';
 
-export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: null
-        };
-        this.getUserName = this.getUserName.bind(this);
-        this.getUserName()
-    }
 
-    async getUserName() {
-        this.setState({refreshing:true})
-        let response = await fetch(
-            'https://api.fikregel.daire101.com/posts/new',
-        );
-        let responseJson = await response.json();
-        if (responseJson) {
-            this.setState({data: responseJson});
-        } else {
-            console.log('User Name Not Found!');
+import React, { useState, useEffect } from 'react';
+import {
+    SafeAreaView,
+    StyleSheet, Text, View
+} from 'react-native';
+import { createAppContainer, createBottomTabNavigator } from 'react-navigation';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
+import { Icon, IconButton } from 'react-native-elements';
+import New from './src/New';
+import Popular from './src/Popular';
+
+
+const TabNavigator = createMaterialBottomTabNavigator({
+    Home: {
+        screen: New,
+        navigationOptions: {
+            tabBarLabel: 'Yeni',
+            activeColor: '#57d997',
+            inactiveColor: '#8b9196',
+            barStyle: { backgroundColor: '#262626' },
+            tabBarIcon: () => (
+                <View style={{display:"flex",justifyContent:"center"}}>
+                </View>
+            )
+        }
+    },
+    Home2: {
+        screen: Popular,
+        navigationOptions: {
+            tabBarLabel: 'Popular',
+            activeColor: '#57d997',
+            inactiveColor: '#8b9196',
+            barStyle: { backgroundColor: '#262626' },
+            tabBarIcon: () => (
+                <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                    
+                </View>
+            )
         }
     }
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <FlatList
-
-                    data={this.state.data}
-                    renderItem={({item}) =>
-                        (<Text>{item.title}</Text>)
-                    }
-                    numColumns={1}
-
-                />
-            </View>
-        );
-    };
-}
-const styles = StyleSheet.create({
-    container: {
-        ...StyleSheet.absoluteFillObject,
-        backgroundColor: '#ffffff',
-    },
-
 });
+
+export default createAppContainer(TabNavigator)
---
+++ src/Popular.js
@@ -0,0 +1,142 @@
+
+import React, { useState, useEffect } from 'react';
+import { Col, Row, Grid } from "react-native-easy-grid";
+import Moment from 'moment';
+
+import {
+    SafeAreaView,
+    StyleSheet,
+    ActivityIndicator,
+    FlatList,
+    Text,
+    View,
+    Button,
+    StatusBar,
+    RefreshControl,
+} from 'react-native';
+Moment.locale('tr');
+const Popular = () => {
+    const [refreshing, setRefreshing] = useState(true);
+    const [dataSource, setDataSource] = useState([]);
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const getData = () => {
+        //Service to get the data from the server to render
+        fetch('https://api.fikregel.daire101.com/posts/')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                setRefreshing(false);
+                setDataSource(responseJson);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+
+    const ItemView = ({ item }) => {
+        return (
+            <Grid onPress={() => getItem(item)} style={{ backgroundColor: '#262626', padding: 5, margin: 0}}>
+                <Col size={10} style={{
+                    justifyContent: 'center',
+                    alignContent: 'center',
+                }}>
+                    <Text style={styles.itemStyle3}>
+                        {item.score}
+
+                    </Text>
+                </Col>
+                <Col size={90}>
+                    <Row>
+                        <Text style={styles.itemStyle}>
+                            {item.title}
+                        </Text>
+                    </Row>
+                    <Row>
+                        <Text style={styles.itemStyle2}>
+                            {item.comments.length + " Yorum  ·  " + Moment(item.created).format('HH:MM  YYYY/MM/DD')}
+                        </Text>
+                    </Row>
+                </Col>
+            </Grid>
+        );
+    };
+
+    const ItemSeparatorView = () => {
+        return (
+            <View
+                style={{
+                    height: 0.8,
+                    width: '100%',
+                    backgroundColor: '#333333'
+                }}
+            />
+        );
+    };
+
+    const getItem = (item) => {
+        alert(item.text);
+    };
+
+    const onRefresh = () => {
+        setDataSource([]);
+        getData();
+    };
+
+    return (
+        <SafeAreaView style={{ flex: 1,backgroundColor:'#262626' }}>
+             
+            <View style={{ justifyContent: 'center', alignItems: 'center', padding: 10, backgroundColor: '#262626' }}>
+                <Text style={{ textAlign: 'center', color: '#57d997', fontSize: 24, fontWeight: 'bold' }}>Fikre Gel</Text>
+            </View>
+            <View style={styles.container}>
+                {refreshing ? <ActivityIndicator /> : null}
+                <FlatList
+                    style={{ margin: 8}}
+                    data={dataSource}
+                    keyExtractor={(item, index) => index.toString()}
+                    ItemSeparatorComponent={ItemSeparatorView}
+                    enableEmptySections={true}
+                    renderItem={ItemView}
+                    refreshControl={
+                        <RefreshControl
+                            refreshing={refreshing}
+                            onRefresh={onRefresh}
+                        />
+                    }
+                />
+            </View>
+            
+           
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        flex: 1,
+        backgroundColor: '#1a1a1a',
+    },
+    itemStyle: {
+        color: '#fff',
+        fontSize: 16,
+        paddingRight: 0,
+        paddingTop:3
+    },
+    itemStyle2: {
+        color: '#8b9196',
+        fontSize: 10,
+        paddingLeft: 0,
+        paddingBottom:3
+    },
+    itemStyle3: {
+        color: '#8b9196',
+        textAlign: 'center',
+        fontSize: 18
+    },
+});
+
+export default Popular;
